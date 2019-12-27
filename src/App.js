ec996d9ff6a23f82da6f4af1e8d5e7f67a34d78f
Aaron Beasley <aaroncb@aaroncb.com>
2019-12-26 23:42:38 -0600

    Completed base project, and stretch goal 1.

--- src/App.js
+++ src/App.js
@@ -7,35 +7,36 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+// Context
+import { ProductContext } from './context/ProductContext';
+import { CartContext } from './context/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
+	const removeItem = item => {
+		setCart(cart.filter(product => product.id !== item));
+	}
+
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={{ cart, removeItem }}>
+			<div className="App">
+				<Navigation/>
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				{/* Routes */}
+				<Route exact path="/" component={Products} />
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+				<Route path="/cart" component={ShoppingCart} />
+			</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
